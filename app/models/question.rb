24e64201d13986ea0170aa9d0394b54f7c87cd46
IgorShum <Shumeikoigor@yandex.ru>
2021-03-01 17:47:49 +0300

    +Модель Answer +Тесты валидаций Question и Answer

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ApplicationRecord
   validates :title, :body, presence: true
+  validates :title, length: { minimum: 6, maximum: 120 }
+  validates :body, length: { minimum: 6, maximum: 240 }
+
+  has_many :answers
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it { should validate_presence_of :title }
-  it { should validate_presence_of :body }
+  describe 'validation' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :body }
+    it { should validate_length_of(:title).is_at_least(6) }
+    it { should validate_length_of(:title).is_at_most(120) }
+    it { should validate_length_of(:body).is_at_least(6) }
+    it { should validate_length_of(:body).is_at_most(240) }
+    it { should have_many(:answers) }
+  end
 end
