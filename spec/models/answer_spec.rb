f3ad5750bb30c6539fd99817830bf2f06a93c843
IgorShum <Shumeikoigor@yandex.ru>
2021-03-01 23:38:15 +0300

    +Исправления

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+
+  describe 'association' do
+    it { should belong_to(:question) }
+  end
+
   describe 'validation' do
     it { should validate_presence_of :body }
-    it { should belong_to(:question) }
     it { should validate_length_of(:body).is_at_least(6) }
     it { should validate_length_of(:body).is_at_most(1200) }
   end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
+
+  describe 'association' do
+    it { should have_many(:answers).dependent(:destroy) }
+  end
+
   describe 'validation' do
     it { should validate_presence_of :title }
     it { should validate_presence_of :body }
-    it { should validate_length_of(:title).is_at_least(6) }
-    it { should validate_length_of(:title).is_at_most(120) }
-    it { should validate_length_of(:body).is_at_least(6) }
-    it { should validate_length_of(:body).is_at_most(240) }
-    it { should have_many(:answers) }
+    it { should validate_length_of(:title).is_at_least(6).is_at_most(120) }
+    it { should validate_length_of(:body).is_at_least(6).is_at_most(240) }
   end
 end
