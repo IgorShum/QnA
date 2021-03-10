5a55426269d4d91af0a5352e4aa1e898779933d5
IgorShum <Shumeikoigor@yandex.ru>
2021-03-10 21:23:27 +0300

    +Тесты: User может написать ответ (на странице вопроса)

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'User can write answer on question page' do
   given(:question) { create(:question) }
   given(:questions) { create_list(:question, 2) }
-  context 'Authenticated user action' do
+  context 'Authenticated user action on page' do
     given(:user) { create(:user) }
     scenario 'Authenticated user' do
       sign_in(user)
@@ -17,6 +17,16 @@ feature 'User can write answer on question page' do
       click_on 'Create Answer'
       expect(page).to have_content 'TestText'
     end
+
+    scenario 'Authenticated user write answer in inline form' do
+      sign_in(user)
+      question
+      visit question_path(question)
+      expect(page).to have_content 'Body'
+      fill_in 'Body', with: 'BodTestText'
+      click_on 'Create Answer'
+      expect(page).to have_content 'BodTestText'
+    end
   end
 
   scenario 'Nonauthenticated user tries to write answer' do
@@ -27,6 +37,15 @@ feature 'User can write answer on question page' do
     expect(page).to have_content question.body
     expect(page).to have_content 'New Answer'
     click_on 'New Answer'
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+
+  scenario 'Nonauthenticated user tries write answer in inline form' do
+    question
+    visit question_path(question)
+    expect(page).to have_content 'Body'
+    fill_in 'Body', with: 'BodTestText'
+    click_on 'Create Answer'
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 end
